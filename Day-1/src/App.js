2ad62c5b659fed5c37cabc076f4ed555d2055ccc
youssefelaz <you.el.azzaoui@gmail.com>
2025-12-10 22:07:15 +0100

    22h07

--- Day-1/src/App.js
+++ Day-1/src/App.js
@@ -1,24 +1,49 @@
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react'
+
+function Scroller(){
+  const [chiffre, setChiffre] = useState('');
   return (
-    <body>
-    
-    <div id="couleur"></div>
-    <div id="slider">
-      <input type='range' id="red" name="red" min="0" max="256"/>
-      <label for="red">Rouge</label>
-      <input type='range' id="green" name="green" min="0" max="256"/>
-      <label for="green">Vert</label>
-      <input type='range' id="blue" name="blue" min="0" max="256"/>
-      <label for="blue">Bleu</label>
-      <input type='range' id="bw" name="bw" min="0" max="256"/>
-      <label for="bw">Opacité</label>
+    <>
+    <div>
+      <input type="range" onChange={e => setChiffre(e.target.value)} min="0" max="256"></input>
+      {chiffre}
     </div>
-    
-    </body>
-    
-  );
+    </>
+    );
 }
 
-export default App;
+// Main App
+export default function App() {
+  const [colors] = useState(Array(3).fill(0))
+  const [color, setColor] = useState("beige")
+  const click = () => { 
+
+    let mix = '#'+'11'+'11'+'11'
+    setColor(mix)
+  }
+
+  useEffect(()=>{
+    document.body.style.backgroundColor = color
+  }, [color])
+
+
+  return (
+    <div className="bg-stone-800" >
+
+      <div>
+        <Scroller value={colors[0]}/>
+        {colors[0]}
+        <Scroller value={colors[1]}/> 
+        {colors[1]}
+        <Scroller value={colors[2]}/> 
+        {colors[2]}
+        <button onClick ={() => click()}>Change color</button>
+        
+      </div>
+    </div>
+  )
+  
+}
+
